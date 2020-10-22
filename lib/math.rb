5af981a3697a8d9bd6f1d0a35d90a6e266ee85c9
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-22 19:49:41 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+ sum = num1+num2
+ sum
 end
 
 def subtraction(num1, num2)
-
+  diff= num1 - num2
+  diff
 end
 
 def division(num1, num2)
-
+  div= num1/num2
+  div
 end
 
 def multiplication(num1, num2)
-
+  times= num1*num2
+  times
 end
 
 def modulo(num1, num2)
-
+  mod = num1%num2
+  mod
 end
 
 def square_root(num)
-
+  sr=Math.sqrt(num)
+  sr
 end
